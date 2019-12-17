54cfbe941ba35ca17b8dac3fb137c6bdbfd12592
TechhDan <tech.dan2689@gmail.com>
2019-12-17 11:52:41 -0500

    Http Serializer

---
+++ lib/Http/HttpException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CHRobinson\Http;
+
+class HttpException extends IOException
+{
+    public $statusCode;
+    public $headers;
+
+    public function __construct($message, $statusCode, $headers)
+    {
+        parent::__construct($message);
+        $this->statusCode = $statusCode;
+        $this->headers = $headers;
+    }
+}
